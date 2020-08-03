583d7505dbe8c78f00058686b5867ba7b7f42e80
Gaurav Saini <sainigaurav.2009@gmail.com>
2020-08-04 05:04:07 +0530

    Fixed an issue with file based producer

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,19 @@ use std::fs;
 use std::fs::File;
 use std::io::{stdin, stdout, BufRead, BufReader, Write};
 use std::path::PathBuf;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
 
-fn worker(brokers: String, f: File, topic_name: String) {
+fn worker(s: &Sender<String>, f: File) {
     let reader = BufReader::new(f);
     reader
         .lines()
         .map(|line| line.unwrap())
         .par_bridge()
-        .for_each_init(
-            || build_producer(brokers.clone()),
-            move |producer, line| match producer
-                .send(BaseRecord::to(&topic_name).payload(&line).key(""))
-            {
-                Ok(_) => (),
-                Err(e) => println!("Error while producing message {:?}", e),
-            },
-        );
-    println!("Done");
-}
-
-fn build_producer(brokers: String) -> BaseProducer {
-    BaseProducer::from_config(
-        ClientConfig::new()
-            .set("bootstrap.servers", &brokers)
-            .set("message.timeout.ms", "5000"),
-    )
-    .expect("Producer creation error")
+        .for_each_with(s.clone(), move |sen, line| {
+            sen.send(line)
+                .expect("Failed to send String over the channel");
+        });
 }
 
 fn main() {
@@ -61,6 +48,13 @@ fn main() {
         brokers,
     );
 
+    let mut producer = BaseProducer::from_config(
+        ClientConfig::new()
+            .set("bootstrap.servers", &brokers)
+            .set("message.timeout.ms", "5000"),
+    )
+    .expect("Producer creation error");
+
     if app.is_present("INPUT_FILE") {
         println!("\nRunning in file mode. Use Ctrl+c to exit.\n");
         rayon::ThreadPoolBuilder::new()
@@ -72,10 +66,11 @@ fn main() {
             fs::canonicalize(&PathBuf::from(app.value_of("INPUT_FILE").unwrap()))
                 .expect("Cannot retrieve absolute path of input file");
         let file_handler = File::open(absolute_file_path).expect("Error while creating the file");
-        worker(brokers, file_handler, String::from(topic));
+        let (sender, receiver) = mpsc::channel();
+        worker(&sender, file_handler);
+        produce(&mut producer, &receiver, topic.to_string());
     } else {
         println!("\nRunning in interactive mode. Use Ctrl+c to exit.\n");
-        let producer = build_producer(brokers);
         loop {
             let mut buf = String::new();
             print!("[{}]> ", topic);
@@ -89,5 +84,17 @@ fn main() {
                 Err(e) => println!("Error while producing message {:?}", e),
             };
         }
+    }
+}
+
+fn produce(producer: &mut BaseProducer, receiver: &Receiver<String>, topic: String) {
+    loop {
+        let producer_data = match receiver.recv() {
+            Ok(v) => v,
+            Err(e) => panic!("Error reading future from receiver: {}", e),
+        };
+        producer
+            .send(BaseRecord::to(&topic).payload(&producer_data).key(""))
+            .unwrap();
     }
 }
